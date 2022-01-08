2b85644855980317092af6a88cc89c6e578340df
NotReallyEight <71493060+NotReallyEight@users.noreply.github.com>
2022-01-08 17:56:36 +0000

    Compile: (2770632) Fix stuff

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ const Client_1 = require("./utils/Client");
 const discord_js_1 = (0, tslib_1.__importDefault)(require("discord.js"));
 const path_1 = require("path");
 const config_1 = require("./config");
+const express_1 = (0, tslib_1.__importDefault)(require("express"));
+// create a basic express server
+const app = (0, express_1.default)();
+app.use((_, res) => {
+    res.redirect("https://notreallyeight.tk");
+});
+app.listen(3000);
 const client = new Client_1.Client({
     intents: [discord_js_1.default.Intents.FLAGS.GUILDS, discord_js_1.default.Intents.FLAGS.GUILD_MESSAGES],
     prefix: config_1.config.prefix,

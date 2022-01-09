f24cb23f8545dec4f601b1767059ef6a131b6823
NotReallyEight <wangzndg@gmail.com>
2022-01-09 20:13:23 +0100

    Edit ping slash command

--- src/commands/slash/ping.ts
+++ src/commands/slash/ping.ts
@@ -1,5 +1,14 @@
 import { SlashCommand } from "../../utils/SlashCommand";
 
-export const command = new SlashCommand("ping", (interaction) => {
-	void interaction.reply("Pong!");
-});
+export const command = new SlashCommand(
+	"ping",
+	(interaction) => {
+		void interaction.reply("Pong!");
+	},
+	undefined,
+	{
+		description: "Pong!",
+		type: 1,
+		defaultPermission: true,
+	}
+);

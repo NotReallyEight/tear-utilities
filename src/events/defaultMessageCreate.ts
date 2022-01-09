284a9e418a2d8fa96041e7708ac70ced03737f5a
NotReallyEight <wangzndg@gmail.com>
2022-01-09 14:34:25 +0100

    Add rules command and add handler for message commands

---
+++ src/events/defaultMessageCreate.ts
@@ -0,0 +1,12 @@
+import { Event } from "../utils/Event";
+import { Logger } from "../utils/Logger";
+
+export const event = new Event("messageCreate", async (client, message) => {
+	try {
+		if (message.author.bot) return;
+
+		await client.processCommand(message);
+	} catch (err: any) {
+		Logger.error(`${(err as Error).name}: ${(err as Error).message}`);
+	}
+});

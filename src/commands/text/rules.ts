988c9a58e753621d2b845d74c46ad864a3ddf124
NotReallyEight <wangzndg@gmail.com>
2022-01-10 11:10:38 +0100

    Fix permissions

--- src/commands/text/rules.ts
+++ src/commands/text/rules.ts
@@ -4,33 +4,42 @@ import { join } from "node:path";
 import { Logger } from "../../utils/Logger";
 import { config } from "../../config";
 
-export const command = new Command("rules", (message) => {
-	try {
-		let rules = "";
-		const readerStream = createReadStream(
-			join(__dirname, "..", "..", "..", "assets", "rules.txt")
-		);
+export const command = new Command(
+	"rules",
+	(message) => {
+		try {
+			let rules = "";
+			const readerStream = createReadStream(
+				join(__dirname, "..", "..", "..", "assets", "rules.txt")
+			);
 
-		readerStream.on("data", (chunk) => {
-			rules += chunk;
-		});
-
-		readerStream.on("end", () => {
-			void message.channel.send({
-				embeds: [
-					{
-						title: "Rules",
-						description: rules,
-						color: config.commandsEmbedsColor,
-					},
-				],
+			readerStream.on("data", (chunk) => {
+				rules += chunk;
 			});
-		});
 
-		readerStream.on("error", (err) => {
-			Logger.error(`${err.name}: ${err.message}`);
-		});
-	} catch (err: any) {
-		Logger.error(`${(err as Error).name}: ${(err as Error).message}`);
+			readerStream.on("end", () => {
+				void message.channel.send({
+					embeds: [
+						{
+							title: "Rules",
+							description: rules,
+							color: config.commandsEmbedsColor,
+						},
+					],
+				});
+			});
+
+			readerStream.on("error", (err) => {
+				Logger.error(`${err.name}: ${err.message}`);
+			});
+		} catch (err: any) {
+			Logger.error(`${(err as Error).name}: ${(err as Error).message}`);
+		}
+	},
+	{
+		custom: (message) => {
+			if (message.author.id !== "489031280147693568") return false;
+			return true;
+		},
 	}
-});
+);
--- src/commands/text/server-info.ts
+++ src/commands/text/server-info.ts
@@ -5,42 +5,51 @@ import { Logger } from "../../utils/Logger";
 import { config } from "../../config";
 import type { APIEmbed } from "discord-api-types";
 
-export const command = new Command("server-info", (message) => {
-	try {
-		let serverInfo = "";
-		const readerStream = createReadStream(
-			join(__dirname, "..", "..", "..", "assets", "server-info.txt")
-		);
+export const command = new Command(
+	"server-info",
+	(message) => {
+		try {
+			let serverInfo = "";
+			const readerStream = createReadStream(
+				join(__dirname, "..", "..", "..", "assets", "server-info.txt")
+			);
 
-		readerStream.on("data", (chunk) => {
-			serverInfo += chunk;
-		});
-
-		readerStream.on("end", () => {
-			const embeds: APIEmbed[] = [];
-			serverInfo.split("---").forEach((info, index) => {
-				if (!index)
-					embeds.push({
-						title: "Server Info",
-						description: info,
-						color: config.commandsEmbedsColor,
-					});
-				else
-					embeds.push({
-						description: info,
-						color: config.commandsEmbedsColor,
-					});
+			readerStream.on("data", (chunk) => {
+				serverInfo += chunk;
 			});
 
-			void message.channel.send({
-				embeds,
-			});
-		});
+			readerStream.on("end", () => {
+				const embeds: APIEmbed[] = [];
+				serverInfo.split("---").forEach((info, index) => {
+					if (!index)
+						embeds.push({
+							title: "Server Info",
+							description: info,
+							color: config.commandsEmbedsColor,
+						});
+					else
+						embeds.push({
+							description: info,
+							color: config.commandsEmbedsColor,
+						});
+				});
 
-		readerStream.on("error", (err) => {
-			Logger.error(`${err.name}: ${err.message}`);
-		});
-	} catch (err: any) {
-		Logger.error(`${(err as Error).name}: ${(err as Error).message}`);
+				void message.channel.send({
+					embeds,
+				});
+			});
+
+			readerStream.on("error", (err) => {
+				Logger.error(`${err.name}: ${err.message}`);
+			});
+		} catch (err: any) {
+			Logger.error(`${(err as Error).name}: ${(err as Error).message}`);
+		}
+	},
+	{
+		custom: (message) => {
+			if (message.author.id !== "489031280147693568") return false;
+			return true;
+		},
 	}
-});
+);

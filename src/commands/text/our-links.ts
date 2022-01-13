31d9ceb639d5eb53d9a358eb2623d15ae85f83fd
NotReallyEight <wangzndg@gmail.com>
2022-01-13 10:53:48 +0100

    Add our-links embed

---
+++ src/commands/text/our-links.ts
@@ -0,0 +1,34 @@
+import { Command } from "../../utils/Command";
+import { Logger } from "../../utils/Logger";
+import { createReadStream } from "node:fs";
+import { join } from "node:path";
+import { config } from "../../config";
+import type { APIEmbed } from "discord-api-types";
+
+export const command = new Command("our-links", (message) => {
+	try {
+		let ourLinks = "";
+		const readerStream = createReadStream(
+			join(__dirname, "..", "..", "..", "assets", "our-links.txt")
+		);
+
+		readerStream.on("data", (chunk) => {
+			ourLinks += chunk;
+		});
+
+		readerStream.on("end", () => {
+			const embeds: APIEmbed[] = [];
+			embeds.push({
+				title: "OUR LINKS",
+				description: ourLinks,
+				color: config.commandsEmbedsColor,
+			});
+			void message.channel.send({
+				embeds,
+			});
+		});
+	} catch (err: any) {
+		Logger.error(`${(err as Error).name}: ${(err as Error).message}`);
+		void message.channel.send(`Error: ${(err as Error).message}`);
+	}
+});

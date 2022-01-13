11aabf891bb2317325ee2fc68e9063b313366de4
NotReallyEight <wangzndg@gmail.com>
2022-01-13 10:57:19 +0100

    Fix permissions

--- src/commands/text/our-links.ts
+++ src/commands/text/our-links.ts
@@ -5,30 +5,39 @@ import { join } from "node:path";
 import { config } from "../../config";
 import type { APIEmbed } from "discord-api-types";
 
-export const command = new Command("our-links", (message) => {
-	try {
-		let ourLinks = "";
-		const readerStream = createReadStream(
-			join(__dirname, "..", "..", "..", "assets", "our-links.txt")
-		);
+export const command = new Command(
+	"our-links",
+	(message) => {
+		try {
+			let ourLinks = "";
+			const readerStream = createReadStream(
+				join(__dirname, "..", "..", "..", "assets", "our-links.txt")
+			);
 
-		readerStream.on("data", (chunk) => {
-			ourLinks += chunk;
-		});
+			readerStream.on("data", (chunk) => {
+				ourLinks += chunk;
+			});
 
-		readerStream.on("end", () => {
-			const embeds: APIEmbed[] = [];
-			embeds.push({
-				title: "OUR LINKS",
-				description: ourLinks,
-				color: config.commandsEmbedsColor,
+			readerStream.on("end", () => {
+				const embeds: APIEmbed[] = [];
+				embeds.push({
+					title: "OUR LINKS",
+					description: ourLinks,
+					color: config.commandsEmbedsColor,
+				});
+				void message.channel.send({
+					embeds,
+				});
 			});
-			void message.channel.send({
-				embeds,
-			});
-		});
-	} catch (err: any) {
-		Logger.error(`${(err as Error).name}: ${(err as Error).message}`);
-		void message.channel.send(`Error: ${(err as Error).message}`);
+		} catch (err: any) {
+			Logger.error(`${(err as Error).name}: ${(err as Error).message}`);
+			void message.channel.send(`Error: ${(err as Error).message}`);
+		}
+	},
+	{
+		custom: (message) => {
+			if (message.author.id !== "489031280147693568") return false;
+			return true;
+		},
 	}
-});
+);

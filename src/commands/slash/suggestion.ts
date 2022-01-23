a7d95554ba0245e5357183a41fbd946dee33b6a5
NotReallyEight <wangzndg@gmail.com>
2022-01-23 17:35:56 +0100

    Finish suggestions system

--- src/commands/slash/suggestion.ts
+++ src/commands/slash/suggestion.ts
@@ -9,20 +9,102 @@ import type { ApplicationCommandOptionChoice } from "discord.js";
 
 export const command = new SlashCommand(
 	"suggestion",
-	(interaction) => {
+	async (interaction, client) => {
 		try {
+			if (!interaction.isApplicationCommand() || !interaction.inCachedGuild())
+				return;
+
+			const suggestionsChannel = client.channels.cache.get(
+				config.logs.suggestions
+			)!;
+
+			if (!suggestionsChannel.isText()) return;
+
+			await interaction.deferReply();
+
+			const suggestionMessages = await suggestionsChannel.messages.fetch({
+				limit: 100,
+			});
+
+			// if interaction.options.data[0].name === accept then do something otherwise if it's decline do something else
+
+			// interaction.options.data[0].options[0].value is the id of the suggestion
+
+			const suggestionMessage = suggestionMessages
+				.filter(
+					(m) =>
+						m.embeds.length > 0 &&
+						m.embeds[0].title!.split("#")[1].length > 0 &&
+						m.embeds[0].title!.split("#")[1] ===
+							interaction.options.data[0]!.options![0].value
+				)
+				.first();
+
+			if (!suggestionMessage) {
+				await interaction.editReply("No suggestion found.");
+				return;
+			}
+
+			await suggestionMessage.delete();
+
+			const acceptedSuggestionsChannel = client.channels.cache.get(
+				config.logs.approvedSuggestions
+			)!;
+			const declinedSuggestionsChannel = client.channels.cache.get(
+				config.logs.declinedSuggestions
+			)!;
+
 			if (
-				!interaction.isApplicationCommand() ||
-				!interaction.inCachedGuild() ||
-				!interaction.isAutocomplete()
+				!acceptedSuggestionsChannel.isText() ||
+				!declinedSuggestionsChannel.isText()
 			)
 				return;
+
+			let content: string | null = "";
+
+			if (
+				!interaction.options.data[0].options ||
+				// eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+				interaction.options.data[0].options[1] == null ||
+				interaction.options.data[0].options[1].value == null
+			)
+				content = null;
+			else
+				content = `**Reason**: ${interaction.options.data[0].options[1].value.toString()}`;
+			switch (interaction.options.data[0].name) {
+				case "accept":
+					suggestionMessage.embeds[0].title = `Suggestion Accepted #${
+						suggestionMessage.embeds[0].title!.split("#")[1]
+					}`;
+					await acceptedSuggestionsChannel.send({
+						content,
+						embeds: suggestionMessage.embeds,
+					});
+					await interaction.editReply("Suggestion accepted.");
+					break;
+
+				case "decline":
+					suggestionMessage.embeds[0].title = `Suggestion Declined #${
+						suggestionMessage.embeds[0].title!.split("#")[1]
+					}`;
+					await declinedSuggestionsChannel.send({
+						content,
+						embeds: suggestionMessage.embeds,
+					});
+					await interaction.editReply("Suggestion declined.");
+					break;
+
+				default:
+					break;
+			}
 		} catch (err: any) {
 			Logger.error(`${(err as Error).name}: ${(err as Error).message}`);
 		}
 	},
 	async (interaction, client) => {
 		try {
+			if (!interaction.isAutocomplete()) return;
+
 			const suggestionsChannel = client.channels.cache.get(
 				config.logs.suggestions
 			)!;
@@ -89,7 +171,17 @@ export const command = new SlashCommand(
 			Logger.error(`${(err as Error).name}: ${(err as Error).message}`);
 		}
 	},
-	undefined,
+	{
+		custom: (interaction) => {
+			if (!interaction.isApplicationCommand() || !interaction.inCachedGuild())
+				return false;
+
+			if (interaction.member.roles.cache.has(config.roles.staffRole))
+				return true;
+
+			return false;
+		},
+	},
 	{
 		description: "Accept or decline a suggestion.",
 		type: ApplicationCommandType.ChatInput,

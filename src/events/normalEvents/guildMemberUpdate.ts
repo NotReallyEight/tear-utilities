018de5964852d0fd844b7b118216bb5e1974cd6a
Eight∞ <wangzndg@gmail.com>
2022-06-20 12:52:09 +0200

    feat: add Angel role only when a user has completed membership screening

---
+++ src/events/normalEvents/guildMemberUpdate.ts
@@ -0,0 +1,20 @@
+import { config } from "../../config";
+import { Event } from "../../utils/Event";
+import { Logger } from "../../utils/Logger";
+
+export const event = new Event(
+	"guildMemberUpdate",
+	async (_client, oldMember, newMember) => {
+		if (!(oldMember.pending && !newMember.pending)) return;
+
+		const role = await newMember.guild.roles.fetch(config.autoRoles.member);
+
+		if (role == null) {
+			Logger.error(`Could not find role ${config.autoRoles.member}`);
+			return;
+		}
+
+		void newMember.roles.add(role);
+	}
+);
+
